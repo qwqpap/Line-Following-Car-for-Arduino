30da5538921cea2ca7e45c5e8021c0c376d856a2
qwqpap <74455027+qwqpap@users.noreply.github.com>
2023-03-12 13:16:28 +0800

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+
 //传感器
 #include "Arduino.h"
 #include "PID_v1.h"
@@ -16,8 +17,7 @@
 #define motor_l_f 9
 #define motor_r_f 10
 #define motor_turn 40
-#define motor_turn_go 50
-
+#define motor_turn_go 30
 //舵机
 #define direc 3
 
@@ -26,21 +26,10 @@
 #define Cycle 10
 
 //以下为直行的参数们
-#define front_motor 90
+#define front_motor 150
 
-//以下为关于直角转弯神秘参数们
-#define turn_front 160
-#define turn_back 160//直角转弯时的电机差速
-#define direc_r 0//左转时的舵机方向（我认为是0
-#define direc_l 255 //直角右转的舵机方向
-#define turn_time 1000//直角转向的具体时间
-//以下为关于锐角转弯的神秘参数
-#define sharp_turn_front 255
-#define sharp_turn_back 0//锐角转弯时的电机差速
-#define sharp_direc_r 0//左转时的舵机方向（我认为是0
-#define sharp_direc_l 255 //锐角角右转的舵机方向
-#define sharp_turn_time 500//锐角转向的具体时间
-#define test 30
+
+#define test 0
 
 //不思进取，只等开源（调用现成的库香到爆
 double door_ji = 0;//舵机的预期值
@@ -51,53 +40,6 @@ double i_pid = 0;//关掉！关掉！一定要关掉！
 double d_pid = 5;
 PID myPID(&fina, &direct_control, &door_ji, p_pid, i_pid, d_pid, REVERSE);
 
-//锐角转弯函数
-void sharp_go_left() {
-    analogWrite(motor_l_f,sharp_turn_back);
-    analogWrite(motor_r,sharp_turn_front);//左电机反转，右边电机正转
-    analogWrite(direc,sharp_direc_r);//舵机控制
-    delay(sharp_turn_time);//执行时间
-}
-//锐角转弯函数
-void sharp_go_right() {
-    analogWrite(motor_l,sharp_turn_front);
-    analogWrite(motor_r_f,sharp_turn_back);//右电机反转，左电机正转
-    analogWrite(direc,sharp_direc_l);//舵机控制
-    delay(sharp_turn_time);//执行时间
-}
-
-
-
-//直角转弯函数
-void go_left() {
-    analogWrite(motor_l_f,turn_back);
-    analogWrite(motor_r,turn_front);//左电机反转，右边电机正转
-    analogWrite(direc,direc_r);//舵机控制
-    delay(turn_time);//执行时间
-}
-//直角转弯函数
-void go_right() {
-    analogWrite(motor_l,turn_front);
-    analogWrite(motor_r_f,turn_back);//右电机反转，左电机正转
-    analogWrite(direc,direc_l);//舵机控制
-    delay(turn_time);//执行时间
-}
-
-
-
-
-
-//把整个数组里的数加起来   积分数组
-int i_count(float values[Cycle]) {
-    int res = 0;
-    for (int i = 0; i < Cycle; i++) {
-        res = res + values[i];
-        //Serial.println(values[i]);
-    }
-    Serial.print("icount:");
-    Serial.println(res);
-    return res;
-}
 //计算pid算法所需偏差值
 double last_value = 128;
 
@@ -127,40 +69,7 @@ double Value_count(int *value) {
     return return_value;
 }
 
-//pid控制   参数：   传感器数组
-void Pid_control(int *value) {
-    static int cycle;  //周期
-    static int last_cycle = Cycle - 1;
-    static float values[Cycle];  //存积分值
-    float directions = 0;        //舵机方向
 
-    //记录积分值
-    values[cycle] = Value_count(value);
-    //pid计算
-    directions = values[cycle] * p_pid + (values[cycle] - values[last_cycle]) * d_pid + i_count(values) * i_pid;
-    //操作
-    if (directions >= 3) {
-        directions = 3;
-    } else if (directions <= -3) {
-        directions = -3;
-    }
-    analogWrite(direc,map(directions, -3, 3, 0, 255));
-    Serial.print("now value count:");
-    Serial.println(values[cycle]);
-    Serial.print("direction:");
-    Serial.println(directions);
-    Serial.print("cycle:");
-    Serial.println(cycle);
-
-
-    //编辑循环
-    last_cycle = cycle;
-    cycle++;
-    //重置循环
-    if (cycle > (Cycle - 1)) {
-        cycle = 0;
-    }
-}
 
 
 
@@ -190,14 +99,14 @@ void Run_motor(int speed_l, int speed_r) {
         if(speed_l > 210){
             speed_l =210;
         }
-        else if(speed_l < 45){
-            speed_l = 45;
+        else if(speed_l < 25){
+            speed_l = 25;
         }
         if(speed_r > 240){
             speed_r =240;
         }
-        else if(speed_r < 45){
-            speed_r = 45;
+        else if(speed_r < 25){
+            speed_r =25;
         }
         analogWrite(motor_l_f, speed_l);
         analogWrite(motor_r, speed_r);
@@ -209,14 +118,14 @@ void Run_motor(int speed_l, int speed_r) {
         if(speed_l > 210){
             speed_l =210;
         }
-        else if(speed_l < 45){
-            speed_l = 45;
+        else if(speed_l < 25){
+            speed_l = 25;
         }
         if(speed_r > 240){
             speed_r =240;
         }
-        else if(speed_r < 45){
-            speed_r = 45;
+        else if(speed_r < 25){
+            speed_r = 25;
         }
         analogWrite(motor_l, speed_l);
         analogWrite(motor_r_f, speed_r);
@@ -273,8 +182,8 @@ void go_front(){
     //int err;
     //Serial.println("----------------------");
     //emmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm这下有得忙了
-    int left_less = 0;
-    int right_less = 0;
+    int left_less = 20;
+    int right_less = 20;
     if(direct_control >= 170){
         right_less = ((direct_control - 170) * motor_turn);
         direct_control = 210;
@@ -289,33 +198,21 @@ void go_front(){
         // Run_motor(front_motor - right_less + left_less,front_motor -left_less + right_less);
         Run_motor(front_motor,front_motor);
     }
-    Run_direct(direct_control);
-
+    /*
+    if(direct_control >= 128){
+      direct_control = 180;
+    }
+    else if(direct_control < 128){
+      direct_control = 50;
+    }
+    */
+  Run_direct(direct_control);
 }
 
 
 int left_out_of_control = 0;
 int right_out_of_control = 0;
-void where_are_you_pid(double error){
-    if(error >= 254&&right_out_of_control<=10){
-        right_out_of_control++;
-        left_out_of_control = 0;
 
-    }
-    else if(error <= 1 &&left_out_of_control<=10){
-        left_out_of_control++;
-        right_out_of_control = 0;
-
-    }
-    else if(error >= 254 && right_out_of_control >10){
-        right_out_of_control = 0;
-        go_right();
-    }
-    else if(error <= 1 && left_out_of_control >10){
-        left_out_of_control = 0;
-        go_left();
-    }
-}
 void setup() {
     Serial.begin(57600);
     //传感器
@@ -328,7 +225,7 @@ void setup() {
     pinMode(sensor_l3, INPUT);
 
     //舵机，左右电机
-    pinMode(direc, OUTPUT);
+    
     pinMode(motor_l, OUTPUT);
     pinMode(motor_r, OUTPUT);
     myPID.SetMode(AUTOMATIC);
@@ -348,6 +245,5 @@ void loop() {
     Serial.println(direct_control);
     free(value);
     go_front();
-    delay(49);
     // put your main code here, to run repeatedly:
 }
